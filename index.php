f3b3e78087b1a39185646a993bc0bc0723dd86c9
Hamza <Hamza5955>
2021-10-14 10:08:40 +0200

    rf

--- index.php
+++ index.php
@@ -7,20 +7,21 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <script src="https://cdn.jsdelivr.net/npm/vue/dist/vue.js"></script>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/axios/0.23.0/axios.min.js" integrity="sha512-Idr7xVNnMWCsgBQscTSCivBNWWH30oo/tzYORviOCrLKmBaRxRflm2miNhTFJNVmXvCtzgms5nlJF4az2hiGnA==" crossorigin="anonymous" referrerpolicy="no-referrer"></script>
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
+    <!-- <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous"> -->
     <link rel="stylesheet" href="css/style.css">
     <title>PHP Dischi</title>
 </head>
 
 <body>
+<div id="root">
    <header>
       <div class="header">
          <img class="img" src="./img/spotify-logo.png" alt="" />
       </div>
    </header>
    <main>
-        <div id="root" class="container">
-
+        <div class="container">
+            
             <!-- 
                 foreach ($db as $disco) {
                     
@@ -54,7 +55,7 @@
             
 
         </div>
-    
+    </div>
    </main>
    <script src="js/main.js"></script>
 </body>
